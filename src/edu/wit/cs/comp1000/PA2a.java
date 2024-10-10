892830f1a4bc7454b563f8930eea738da992f534
nikom <nikom@NikosSkoolPC.myfiosgateway.com>
2024-10-10 11:34:36 -0400

    added scanner, aswell as int codes and system.out

--- src/edu/wit/cs/comp1000/PA2a.java
+++ src/edu/wit/cs/comp1000/PA2a.java
@@ -1,11 +1,23 @@
 package edu.wit.cs.comp1000;
 
-// TODO: document this class
+import java.util.Scanner;
+
 public class PA2a {
 
-	// TODO: document this function
 	public static void main(String[] args) {
-		// TODO: write your code here
+		Scanner scanner = new Scanner(System.in);
+		
+		int sumPositive =0, sumNonPositive =0, sumAll = 0;
+		int countPositive = 0, countNonPositive = 0;
+		
+		//Asks user for input
+		System.out.println("Enter five whole numbers");
+		
+		// Gets five numbers from user
+		for (int i = 0; i < 5; i++) {
+			int num = scanner.nextInt();
+			
+		}
 	}
 
 }
