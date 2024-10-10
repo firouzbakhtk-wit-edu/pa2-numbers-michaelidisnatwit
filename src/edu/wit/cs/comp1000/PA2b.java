f8ad94b896e68c66c82b1a750cb6d44591750625
Niko Michaelidis <michaelidisn@wit.edu>
2024-10-10 13:08:03 -0400

    added two real roots and correct order

--- src/edu/wit/cs/comp1000/PA2b.java
+++ src/edu/wit/cs/comp1000/PA2b.java
@@ -26,6 +26,17 @@ public class PA2b {
 			double root = -b / (2 * a);
 			System.out.printf("Root: %.2f%n", root);
 		} else {
+			
+			// Two real roots
+			double root1 = (-b - Math.sqrt(discriminant)) / (2 * a);
+			double root2 = (-b + Math.sqrt(discriminant)) / (2 * a);
+			
+			// Ensures the roots are printed in the correct order
+			 if root1 > root2) {
+				double temp = root1;
+				root1 = root2;
+				root2 = temp;
+			}
 		}
 	}
 
