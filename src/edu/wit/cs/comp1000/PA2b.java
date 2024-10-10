078a19f0676368ce046e1635149ca384abf71a09
Niko Michaelidis <michaelidisn@wit.edu>
2024-10-10 13:02:16 -0400

    added handles cases based on the discrimiant and one real root

--- src/edu/wit/cs/comp1000/PA2b.java
+++ src/edu/wit/cs/comp1000/PA2b.java
@@ -17,7 +17,16 @@ public class PA2b {
 		//Calculates discriminant
 		double discriminant = b * b - 4 * a * c;
 		
-		
+		// handles cases based on the discriminant
+		if (discriminant < 0) {
+			System.out.println("Root: imaginary");
+		} else if (discriminant == 0) {
+			
+			// one real root
+			double root = -b / (2 * a);
+			System.out.printf("Root: %.2f%n", root);
+		} else {
+		}
 	}
 
 }
