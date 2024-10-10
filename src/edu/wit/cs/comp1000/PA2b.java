31fd59f96787dd2366269f738f26fc90e2a09110
Niko Michaelidis <michaelidisn@wit.edu>
2024-10-10 12:50:23 -0400

    added Asks user for input

--- src/edu/wit/cs/comp1000/PA2b.java
+++ src/edu/wit/cs/comp1000/PA2b.java
@@ -1,11 +1,19 @@
 package edu.wit.cs.comp1000;
 
+import java.util.Scanner;
 
 public class PA2b {
 
 	
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
+		
+		//Asks user for input
+		System.out.println("Enter a b c:");
+		double a = scanner.nextDouble();
+		double b = scanner.nextDouble();
+		double c = scanner.nextDouble();
+		
 		
 	}
 
